1837d8ab1684be2a9c2dc5dea4adfdc87c4395fc
Paul Chevilley <paul.chevilley@gmail.com>
2023-08-08 14:07:50 -0400

    add nvim config

---
+++ nvim/after/plugin/colors.lua
@@ -0,0 +1,11 @@
+vim.cmd.colorscheme("horizon")
+
+-- require('rose-pine').setup({
+-- 	variant = 'moon'
+-- })
+
+-- vim.cmd.colorscheme("rose-pine")
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+
